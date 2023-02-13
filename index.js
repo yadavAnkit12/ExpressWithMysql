ab823541ff7004deb8058e0bed5aa8b0f50c9193
yadavAnkit12 <ay1222926@gmail.com>
2023-02-13 18:14:59 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express=require("express")
 const app=express()
+const db=require("./model/connection")
 
 app.listen(7000,()=>console.log("connected"))
+
+app.use(express.json())   // midlleware 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
